8fc0825a3edf8082185427e41b317b03f96b782f
Leonayo <nayo0710@gmail.com>
2021-09-03 19:39:56 -0500

    testinicial

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+
+let count = 1;
+
+while (count < 50) {
+    console.log(count + (' Funciona');
+    count = count + 1;
+}
+
+// Para usar Comentarios Largos o infinitos recuerda usar /* $$ */
+// > Significa Mayor que
+// < Menor Que
+// == Igual que
+// != Es diferenre a ?
+// <= Menor o igual que
+// >= Mayor o igual que
+// else if significa En Caso Contrario Muestra... 
+// else Significa s ninguna de estas 2 pasa muestra esto
+// ctrl + A = Selecciona Todo
+// Ctrl + N = Abre una ventana nueva
+// Ctrl + O = Para Abrir Un Nuevo Archivo
+// ctrl + B = Abre las Opciones o Cierra Opciones de la ventna izquierda
+// ctrl + Q = Abre Opciones. Se elegen al soltar los botones
+// ctrl + k + Ctrl + o = Abre una Nueva Carpeta
+// crtl + Shift + L = Selecciona Toda las palabras Ocurrentes () 
+// crtl + L = Selecciona toda la linea
+// ctrl + h = ENcuentra y selecciona y remplaza., Potencialmente util
+// ctrl + D = Potencialmente util. Seleciona las palabras Ocurrentes, las mismas y las puede seleccionar y remplazar
+// Shift + alt + Arriba o Abajo = Copia la Linea. Potenciamnente util
